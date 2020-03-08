3e6d457b3dcc14a5c40499e0f0b7f44ab80a5be6
Justsnoopy30 <everettallen30@gmail.com>
2020-03-08 15:22:25 -0500

    Improve updateUpstream.sh and Update Upstream (Waterfall)

--- scripts/updateUpstream.sh
+++ scripts/updateUpstream.sh
@@ -3,13 +3,19 @@
 echo Merging upstream...
 ./hyperfall m
 echo Merged upstream!
-read -rsp $'Press enter to continue...\n'
+read -rsp $'Press enter to continue or Ctrl+C to exit...\n'
 echo Applying all the patches to Waterfall...
 ./hyperfall p
 echo Applied all the patches to Waterfall!
-echo Check to see if there is any conflicts and fix them if there are by fixing it in HyperFall-Proxy and running git am --continue.
-read -rsp $'Press enter to continue...\n'
+echo Check to see if there is any conflicts and fix them if there are any with the steps below:
+echo 1) Ctrl+C to exit
+echo 2) Fix the merge conflict in HyperFall-Proxy
+echo 3) cd HyperFall-Proxy
+echo 4) git am --continue
+echo 5) cd ..
+echo 6) ./hyperfall rb
+read -rsp $'Press enter to continue or Ctrl+C to exit...\n'
 echo Rebuilding the patches...
 ./hyperfall rb
 echo Rebuilt the patches!
-read -rsp $'Press enter to continue...\n'
+read -rsp $'Press enter to continue or Ctrl+C to exit...\n'
